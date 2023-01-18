e3f542742d1ebf408827ce1c1cb685aaa31ba385
Wilsson Castillo <dc105677543@gmail.com>
2023-01-18 11:54:02 -0500

    first commit

---
+++ cicloarraryszx.js
@@ -0,0 +1,5 @@
+ let nums = [2,11,15,3];
+for(i = 0; i<nums.length; i++){
+    if(nums[i]>10)
+    console.log(nums[i]);
+}
---
+++ tem.js
@@ -0,0 +1,17 @@
+let nums = [1,2,3,1,4,5,5,4,8,7]
+let itemsearch= prompt ("ingrese numero")
+let cont = 0;
+for(let i=0; i<nums.length; i++) {
+    if (nums[i]==itemsearch){
+        cont++;
+
+
+
+
+
+
+    }
+
+
+}
+console.log("se encontraron ",cont)
