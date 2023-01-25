8fc2a9025aaa8f763cd6578271b6dd3918d41231
Wilson8jsn <dc105677543@gmail.com>
2023-01-25 12:08:09 -0500

    first commit

--- tem.js
+++ tem.js
@@ -1,7 +1,7 @@
 let nums = [1,2,3,1,4,5,5,4,8,7]
 let itemsearch= prompt ("ingrese numero")
 let cont = 0;
-for(let i=0; i<nums.length; i++) {
+for(let i=0; i < nums.length; i++) {
     if (nums[i]==itemsearch){
         cont++;
 
